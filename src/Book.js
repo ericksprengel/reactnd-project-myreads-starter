2c5aae9c420e44a32e58e29ea207f2d822ca2929
Erick Massa Sprengel <erick.sprengel@gmail.com>
2019-01-01 19:49:08 -0200

    refactor: extract Book component

---
+++ src/Book.js
@@ -0,0 +1,46 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+
+class Book extends React.Component {
+  render() {
+    const { book } = this.props
+    const bookCoverStyle = {
+      width: 128,
+      height: 193,
+      backgroundImage: `url(${book.imageLinks.thumbnail})`,
+    }
+    return (
+      <div className="book">
+        <div className="book-top">
+          <div
+            className="book-cover"
+            style={bookCoverStyle}></div>
+          <div className="book-shelf-changer">
+            <select>
+              <option value="move" disabled>Move to...</option>
+              <option value="currentlyReading">Currently Reading</option>
+              <option value="wantToRead">Want to Read</option>
+              <option value="read">Read</option>
+              <option value="none">None</option>
+            </select>
+          </div>
+        </div>
+        <div className="book-title">{book.title}</div>
+        <div className="book-authors">{book.authors.join(", ")}</div>
+      </div>
+    )
+  }
+}
+
+Book.propTypes = {
+  book: PropTypes.shape({
+    title: PropTypes.string.isRequired,
+    authors: PropTypes.arrayOf(PropTypes.string),
+    imageLinks: PropTypes.shape({
+      thumbnail: PropTypes.string.isRequired,
+    }),
+  })
+}
+
+export default Book
